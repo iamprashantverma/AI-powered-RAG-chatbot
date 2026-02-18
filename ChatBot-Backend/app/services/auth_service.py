d92ac6be30d356a72aa2ae3c0b17149d9aef360e
iamprashantverma <prashant2k26@gmail.com>
2026-02-18 16:54:47 +0530

    feat : auth service implemented

---
+++ ChatBot-Backend/app/services/auth_service.py
@@ -0,0 +1,29 @@
+from app.schemas.auth import LoginRequest, LoginResponse
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
+from app.core.security import verify_password
+from app.crud.user import get_user_by_email
+from app.core.security import verify_password
+from app.core.jwt import create_access_token  
+
+def login_service(db: Session, login_cred: LoginRequest):
+    user = get_user_by_email(db, login_cred.email)
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalid email"
+        )
+
+    if not verify_password(login_cred.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid password"
+        )
+
+    token = create_access_token(data={"sub": user.email})
+
+    return LoginResponse(
+        message=f"Login Successfully {user.email}",
+        access_token=token
+    )
