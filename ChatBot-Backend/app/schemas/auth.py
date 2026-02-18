513c81664f9a0b4fdc530e00fdea8d84b036a482
iamprashantverma <prashant2k26@gmail.com>
2026-02-18 16:53:54 +0530

    feat: auth, chat schema's added

---
+++ ChatBot-Backend/app/schemas/auth.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+class LoginRequest(BaseModel):
+    email: EmailStr = Field(
+        ...,
+        description="Registered user email",
+        examples=["prashant@example.com"]
+    )
+    password: str = Field(
+        ...,
+        description="User password",
+        examples=["Test@1234"]
+    )
+
+
+class LoginResponse(BaseModel):
+    message: str
+    access_token: str
+    token_type: str = "bearer"
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    user_id: str | None = None
