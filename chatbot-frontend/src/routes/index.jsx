346c9f3b8747cb949af73603874e44d8b8f212ee
iamprashantverma <prashant2k26@gmail.com>
2026-02-19 13:18:51 +0530

    feat: index and protected routes added

---
+++ chatbot-frontend/src/routes/index.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
+import Chatbot from '../pages/Chatbot';
+import ProtectedRoute from './ProtectedRoute';
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/chat" 
+        element={
+          <ProtectedRoute>
+            <Chatbot />
+          </ProtectedRoute>
+        } 
+      />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
