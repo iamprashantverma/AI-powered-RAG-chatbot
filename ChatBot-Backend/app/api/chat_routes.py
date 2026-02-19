621aea9b793505ed8f1190652e93d2bdc8a7d1e0
iamprashantverma <prashant2k26@gmail.com>
2026-02-19 13:21:16 +0530

    feat: getHistory feature added

--- ChatBot-Backend/app/api/chat_routes.py
+++ ChatBot-Backend/app/api/chat_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from app.schemas.chat import ChatRequest
-from app.services.chat_service import chatbot
+from app.services.chat_service import chatbot,get_user_history
 from app.rag.retriever import retriever
 from app.api.deps import get_current_user
 from app.models.user import User
@@ -19,6 +19,7 @@ async def chat(req: ChatRequest,current_user: User = Depends(get_current_user)):
         {
             "input": req.message,
             "context": context,
+             "instruction": "Respond ONLY in valid JSON with keys: steps and final_answer"
         },
         config={
             "configurable": {
@@ -26,5 +27,10 @@ async def chat(req: ChatRequest,current_user: User = Depends(get_current_user)):
             }
         }
     )
-
     return {"reply": response.content}
+
+@router.get("")
+async def get_chat_history(current_user: User = Depends(get_current_user)):
+    user_email = current_user["sub"]
+    return  get_user_history(user_email)
+ 
