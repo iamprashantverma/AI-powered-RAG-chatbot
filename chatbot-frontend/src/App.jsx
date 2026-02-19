b850ff18490af6861c9d6a272defde122b3790b1
iamprashantverma <prashant2k26@gmail.com>
2026-02-19 14:37:11 +0530

    refactor:improve the modularity

--- chatbot-frontend/src/App.jsx
+++ chatbot-frontend/src/App.jsx
@@ -1,10 +1,23 @@
-import "./style/main.scss";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import AuthProvider from './contexts/AuthProvider';
+import AppRoutes from './routes';
+import 'react-toastify/dist/ReactToastify.css';
+import './style/main.scss';
+
 function App() {
   return (
-    <>
-      <h1> App is Running</h1>
-    </>
-  )
+    <Router>
+      <AuthProvider>
+        <AppRoutes />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          newestOnTop={false}
+        />
+      </AuthProvider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
