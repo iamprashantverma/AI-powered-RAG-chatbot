ec7cf8a38e71f612bea39af7be01ae5d094cfd24
iamprashantverma <prashant2k26@gmail.com>
2026-02-19 13:23:50 +0530

    fix : cors issues fixed

--- ChatBot-Backend/app/main.py
+++ ChatBot-Backend/app/main.py
@@ -1,5 +1,20 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.router import api_router
+from app.db.base import Base
+from app.db.session import engine 
 
 app = FastAPI()
+origins = ["http://localhost:5173"]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,        
+    allow_credentials=True,
+    allow_methods=["*"],        
+    allow_headers=["*"],
+)
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(api_router)
